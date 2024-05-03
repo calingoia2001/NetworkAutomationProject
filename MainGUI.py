3c4b9de06a3873e7855b72b2ee9884e1ab6ae86d
calingoia <calingoia01@yahoo.com>
2024-05-03 12:21:23 +0300

    add comments to MainGUI.py

--- MainGUI.py
+++ MainGUI.py
@@ -6,47 +6,42 @@ import subprocess
 # Create the main window of GUI
 
 root = Tk()
-root.title("Network Automation Project")
-root.iconbitmap('Assets/gui_icon.ico')
-root.geometry("400x200")
+root.title("Network Automation Project")              # GUI title
+root.iconbitmap('Assets/gui_icon.ico')                # GUI icon
+root.geometry("400x200")                              # GUI size
 
 
-# Call backupConfig script and store the output in the variable result
+# Call backupConfig script and store the output in the variable result then show a message box with the result
 def run_script_backupconfig():
 
     result = subprocess.check_output(["D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/.venv/Scripts/python.exe",
                                       "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/backupConfigScript.py", "calin", "cisco", "router"])
-    # result_label_cfg.config(text=result.decode('utf-8'))  # Display the result in the label
     messagebox.showinfo("Backup configuration state", result.decode('utf-8'))
 
 
+# Call showDataByFilter script and store the output in the variable result then show a message box with the result
 def run_script_showdata():
 
     result = subprocess.check_output(["D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/.venv/Scripts/python.exe",
                                       "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/showDataByFilterScript.py", "calin", "cisco", "switch"])
-    # result_label_cfg.config(text=result.decode('utf-8'))  # Display the result in the label
     messagebox.showinfo("Show data", result.decode('utf-8'))
 
 
+# Call testConnectionWithPing script and store the output in the variable result then show a message box with the result
 def run_script_testconnection():
 
     result = subprocess.check_output(["D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/.venv/Scripts/python.exe",
                                       "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/testConnectionWithPingScript.py", "calin", "cisco", "coresw"])
-    # result_label_cfg.config(text=result.decode('utf-8'))  # Display the result in the label
     messagebox.showinfo("Test connection", result.decode('utf-8'))
 
 
+# Call textFSM script and store the output in the variable result then show a message box with the result
 def run_script_fsmtest():
 
     result = subprocess.check_output(["D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/.venv/Scripts/python.exe",
                                       "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/textFSM.py", "calin", "cisco", "router"])
-    # result_label_cfg.config(text=result.decode('utf-8'))  # Display the result in the label
     messagebox.showinfo("FSM test", result.decode('utf-8'))
 
-
-# Create a label to display the result
-# result_label_cfg = Label(root, text="")
-# result_label_cfg.pack(pady=10)
 
 # Create a button to run the backupConfig script
 button_config = Button(root, text="Backup running configuration", command=run_script_backupconfig)
@@ -63,5 +58,6 @@ button_showData.pack(pady=10)
 # Create a button for textFSM script
 button_showData = Button(root, text="FSM test", command=run_script_fsmtest)
 button_showData.pack(pady=10)
+
 # Run the Tkinter event loop
 root.mainloop()
