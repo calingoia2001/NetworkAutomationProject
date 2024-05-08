d21205045331386c8e8cfd01c967bda56d82682f
calingoia <calingoia01@yahoo.com>
2024-05-08 20:24:40 +0300

    add configure devices window and functionality

--- MainGUI.py
+++ MainGUI.py
@@ -7,9 +7,11 @@ import subprocess
 global device
 global device_2
 global device_3
+global device_4
 global backup_window
 global showdata_window
 global pingtest_window
+global configure_window
 
 # Create the main window of GUI
 root = Tk()
@@ -81,15 +83,17 @@ def run_script_showdata(device_type, show_command):
     messagebox.showinfo("Show data", result.decode('utf-8'))
 
 
+# Function to go back to main menu from the showdata window
 def goback_2():
     showdata_window.destroy()
     root.deiconify()
 
 
+# Display the showdata window
 def create_showdata_window():
     # Create showdata window
     root.withdraw()  # withdraw the main menu
-    global showdata_window  # make showdata_window global ( to be used in goback() function )
+    global showdata_window  # make showdata_window global ( to be used in goback()_2 function )
     showdata_window = Toplevel()  # need to use Toplevel() for a window that opens on another one
     showdata_window.title("Show Data of Devices")  # GUI title
     showdata_window.iconbitmap('Assets/gui_icon.ico')  # GUI icon
@@ -138,15 +142,17 @@ def run_script_testconnection(device_type):
     messagebox.showinfo("Test connection", result.decode('utf-8'))
 
 
+# Function to go back to main menu from the pingtest window
 def goback_3():
     pingtest_window.destroy()
     root.deiconify()
 
 
+# Display the showdata window
 def create_pingtest_window():
     # Create pingtest window
     root.withdraw()  # withdraw the main menu
-    global pingtest_window  # make showdata_window global ( to be used in goback() function )
+    global pingtest_window  # make pingtest_window global ( to be used in goback()_3 function )
     pingtest_window = Toplevel()  # need to use Toplevel() for a window that opens on another one
     pingtest_window.title("Test Connection With Ping")  # GUI title
     pingtest_window.iconbitmap('Assets/gui_icon.ico')  # GUI icon
@@ -181,6 +187,60 @@ def create_pingtest_window():
     button_goback.pack(pady=10)
 
 
+# Call addNewConfig script and show a message with the result
+def run_script_configure(device_type):
+    result = subprocess.check_output(
+        ["D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/.venv/Scripts/python.exe",
+         "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/addNewConfigScript.py",
+         "calin", "cisco", device_type])
+    messagebox.showinfo("Configure Device", result.decode('utf-8'))
+
+
+# Function to go back to main menu from the configure window
+def goback_4():
+    configure_window.destroy()
+    root.deiconify()
+
+
+# Display the configure window
+def create_configure_window():
+    # Create configure window
+    root.withdraw()  # withdraw the main menu
+    global configure_window  # make configure_window global ( to be used in goback()_4 function )
+    configure_window = Toplevel()  # need to use Toplevel() for a window that opens on another one
+    configure_window.title("Configure Devices")  # GUI title
+    configure_window.iconbitmap('Assets/gui_icon.ico')  # GUI icon
+    configure_window.geometry("400x300")  # GUI size
+
+    # Create select text
+    select_text = Label(configure_window, text='Select which device you want to configure', font=50)
+    select_text.pack()
+
+    # Create a list of devices
+    devices = [
+        ("coresw", "coresw"),
+        ("router", "router"),
+        ("switch", "switch"),
+    ]
+
+    global device_4
+    device_4 = StringVar()
+    device_4.set("coresw")  # default value for device type
+
+    # Loop trough list to create radio buttons based on the list
+    for radiobutton_text, value_text in devices:
+        Radiobutton(configure_window, text=radiobutton_text, variable=device_4, value=value_text).pack()
+
+    # Create a button to run the addNewConfig script
+    button_configuration = Button(configure_window, text="Configure Devices",
+                                  command=lambda: run_script_configure(device_4.get()))
+    button_configuration.pack(pady=10)
+
+    # Create a button to go back to main menu
+    button_goback = Button(configure_window, text="Go back", command=goback_4)
+    button_goback.pack(pady=10)
+
+
 # Create select button text
 select_button_text = Label(root, text='Select a task', font=50)
 select_button_text.pack(pady=10)
@@ -198,7 +258,7 @@ button_pingtest = Button(root, text="Test Connection With Ping", command=create_
 button_pingtest.pack(pady=10)
 
 # Create a button for addNewConfig script
-button_configure = Button(root, text="Configure Devices", command=create_pingtest_window)
+button_configure = Button(root, text="Configure Devices", command=create_configure_window)
 button_configure.pack(pady=10)
 
 # Create a button to close the app
--- NornirScripts/addNewConfigScript.py
+++ NornirScripts/addNewConfigScript.py
@@ -1,11 +1,11 @@
-# We use this script to add a new configuration to existing devices that is located in config_file.txt
-# ip scp server enable must be enabled on devices !
+# A script to add a new configuration to existing devices that is located in config_file.txt
+# ip scp server enable must be enabled on devices for this to work!
 import sys
 from nornir import InitNornir
 from nornir_napalm.plugins.tasks import napalm_configure
 from nornir_utils.plugins.functions import print_result
 
-nr = InitNornir(config_file="config.yaml")        # init the config.yaml
+nr = InitNornir(config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config.yaml")  # init the config.yaml
 
 for host in nr.inventory.hosts.values():          # use sys arg to enter username and password
     host.username = sys.argv[1]
@@ -13,9 +13,9 @@ for host in nr.inventory.hosts.values():          # use sys arg to enter usernam
 
 
 def send_config(task):
-    task.run(task=napalm_configure, filename="config_file.txt")
+    task.run(task=napalm_configure, filename="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config_file.txt")
 
 
-nr_filter = nr.filter(type="router")                         # choose "switch or "coresw" or "router"
+nr_filter = nr.filter(type=sys.argv[3])                         # choose "switch or "coresw" or "router"
 results = nr_filter.run(task=send_config)
 print_result(results)
