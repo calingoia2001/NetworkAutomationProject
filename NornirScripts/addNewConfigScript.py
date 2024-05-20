455349428113612d1f6e879f08dd9d3631e7f4db
calingoia <calingoia01@yahoo.com>
2024-05-20 23:45:08 +0300

    refactor addNewConfigScript.py

--- NornirScripts/addNewConfigScript.py
+++ NornirScripts/addNewConfigScript.py
@@ -5,8 +5,14 @@ import ipaddress
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_config                 # without wr
 from nornir_netmiko.tasks import netmiko_save_config                 # save current configuration
+from nornir.core.exceptions import NornirExecutionError
 
-nr = InitNornir(config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config.yaml")  # init the config.yaml
+try:
+    nr = InitNornir(config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config.yaml")  # init the config.yaml
+except FileNotFoundError as e:
+    print(f"Config file not found: {e}")
+except Exception as e:
+    print(f"Failed to initialize Nornir: {e}")
 
 for host_name in nr.inventory.hosts.values():  # use sys arg to enter username and password
     host_name.username = sys.argv[1]
@@ -23,49 +29,58 @@ def check_if_is_ip_address(ip):
 
 
 def send_config(task):
-    if sys.argv[4] == "loopback":               # add loopback
-        task.run(task=netmiko_send_config, config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config_file_loopback.txt")
-        print(f"Loopback interface for {sys.argv[3]} created successfully!\n")
+    try:
+        if sys.argv[4] == "loopback":               # add loopback
+            task.run(task=netmiko_send_config, config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config_file_loopback.txt")
+            print(f"Loopback interface for {sys.argv[3]} created successfully!\n")
 
-    if sys.argv[4] == "noloopback":            # remove loopback
-        task.run(task=netmiko_send_config, config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config_file_remove_loopback.txt")
-        print(f"Loopback interface for {sys.argv[3]} deleted successfully!\n")
+        if sys.argv[4] == "noloopback":            # remove loopback
+            task.run(task=netmiko_send_config, config_file="D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config_file_remove_loopback.txt")
+            print(f"Loopback interface for {sys.argv[3]} deleted successfully!\n")
 
-    if sys.argv[4] == "vlan":                   # add VLANs
-        if sys.argv[3] == "router":
-            print("Cannot create VLANs for a router!")
-        else:
-            print(f"Creating VLANs for {sys.argv[3]} ... \n")
-            for n in range(2, 4):
-                print(f"Creating VLAN {str(n)}")
-                config_command = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
-                task.run(task=netmiko_send_config, config_commands=config_command)
+        if sys.argv[4] == "vlan":                   # add VLANs
+            if sys.argv[3] == "router":
+                print("Cannot create VLANs for a router!")
+            else:
+                print(f"Creating VLANs for {sys.argv[3]} ... \n")
+                for n in range(2, 4):
+                    print(f"Creating VLAN {str(n)}")
+                    config_command = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+                    task.run(task=netmiko_send_config, config_commands=config_command)
 
-    if sys.argv[4] == "novlan":                 # remove VLANs
-        if sys.argv[3] == "router":
-            print("Cannot delete VLANs for a router!")
-        else:
-            print(f"Deleting VLANs for {sys.argv[3]} ... \n")
-            for n in range(2, 4):
-                print(f"Deleting VLAN {str(n)}")
-                config_command = ['no vlan ' + str(n)]
-                task.run(task=netmiko_send_config, config_commands=config_command)
+        if sys.argv[4] == "novlan":                 # remove VLANs
+            if sys.argv[3] == "router":
+                print("Cannot delete VLANs for a router!")
+            else:
+                print(f"Deleting VLANs for {sys.argv[3]} ... \n")
+                for n in range(2, 4):
+                    print(f"Deleting VLAN {str(n)}")
+                    config_command = ['no vlan ' + str(n)]
+                    task.run(task=netmiko_send_config, config_commands=config_command)
 
-    if sys.argv[4] == "restore":               # restore configuration
-        task.run(task=netmiko_send_config, config_file=sys.argv[5], read_timeout=60)
-        print(f"The configuration of {sys.argv[3]} has been successfully restored!\n")
+        if sys.argv[4] == "restore":               # restore configuration
+            task.run(task=netmiko_send_config, config_file=sys.argv[5], read_timeout=60)
+            print(f"The configuration of {sys.argv[3]} has been successfully restored!\n")
 
-    if sys.argv[4] == "saveconfig":            # save configuration
-        task.run(task=netmiko_save_config)
-        print(f"The configuration of {sys.argv[3]} has been successfully saved!\n")
+        if sys.argv[4] == "saveconfig":            # save configuration
+            task.run(task=netmiko_save_config)
+            print(f"The configuration of {sys.argv[3]} has been successfully saved!\n")
+    except NornirExecutionError as err:
+        print(f"Failed to run task on {task.host.name}: {err}")
+    except Exception as err:
+        print(f"Error during backup for {task.host.name}: {err}")
 
 
-if check_if_is_ip_address(sys.argv[3]):                       # check if the ip address is valid
-    nr_filter = nr.filter(filter_func=lambda host: host.hostname == sys.argv[3])      # run sendconfig task on specified ip
-    results = nr_filter.run(task=send_config)  # run task
-else:
-    if sys.argv[3] == "switch" or sys.argv[3] == "router" or sys.argv[3] == "coresw":
-        nr_filter = nr.filter(type=sys.argv[3])                     # filter by switch ("switch" or "coresw" or "router")
+if __name__ == "__main__":
+
+    target = sys.argv[3]
+
+    if check_if_is_ip_address(target):                       # check if the ip address is valid
+        nr_filter = nr.filter(filter_func=lambda host: host.hostname == target)      # run sendconfig task on specified ip
         results = nr_filter.run(task=send_config)  # run task
     else:
-        print("Please enter a valid IP address!")
+        if target in ["switch", "router", "coresw"]:
+            nr_filter = nr.filter(type=target)                     # filter by switch ("switch" or "coresw" or "router")
+            results = nr_filter.run(task=send_config)  # run task
+        else:
+            print("Please enter a valid IP address!")
