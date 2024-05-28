4d2e1c902f72636258492f5f59b77ff570f467ba
calingoia <calingoia01@yahoo.com>
2024-05-28 21:43:53 +0300

    update complianceCheckConfigurationScript.py

--- NornirScripts/complianceCheckConfigurationScript.py
+++ NornirScripts/complianceCheckConfigurationScript.py
@@ -2,10 +2,13 @@
 This script is designed to:
     -> automate compliance check of configuration of network devices using Nornir Napalm validate task
 """
+import os
 import sys
+import threading
 from nornir import InitNornir
 from nornir_napalm.plugins.tasks import napalm_get
-from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.functions import print_title
+from colorama import Fore, Style
 
 CONFIG_PATH = "D:/Programs/PyCharm Community/Python PyCharm Projects/NetworkAutomationProject/NornirScripts/config.yaml"
 
@@ -22,16 +25,37 @@ def initialize_nornir():
         sys.exit(1)
 
 
-def nornir_napalm_get(task):
-    task.run(task=napalm_get, getters=["interfaces"])
+def runner(task):
+    mylist = []
+    result = task.run(task=napalm_get, getters=["get_config"])  # use get_config getter
+    running_config = result[0].result["get_config"]["running"]  # store the running config
+
+    for cmd in filelines:
+        if not cmd in running_config:
+            mylist.append(cmd)
+    if not mylist:
+        print(Fore.YELLOW + f"{task.host} VALIDATED!")
+    else:
+        LOCK.acquire()
+        print(f"WARNING: {task.host} is not compliant!")
+        print("The following commands are missing:")
+        try:
+            for items in mylist:
+                print(items)
+        finally:
+            LOCK.release()
 
 
 if __name__ == "__main__":
+    LOCK = threading.Lock()
     nr = initialize_nornir()
 
     for host_name in nr.inventory.hosts.values():  # add username and password to hosts
         host_name.username = sys.argv[1]
         host_name.password = sys.argv[2]
 
-    results = nr.run(task=nornir_napalm_get)
-    print_result(results)
+    with open('reader.txt', 'r') as f:
+        filelines = f.readlines()
+
+    results = nr.run(task=runner)
+    print_title("COMPLETED COMPLIANCE TEST")
